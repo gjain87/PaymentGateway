81be2b4dd0a71136bc4bb65149cd7437646c38d0
Gaurav Jain <78154992+gjain87@users.noreply.github.com>
2023-12-31 09:54:24 +0530

    Add files via upload

---
+++ src/main/java/com/gateway/controller/HomeController.java
@@ -0,0 +1,46 @@
+package com.gateway.controller;
+
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.razorpay.*;
+
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String welcome()
+	{
+		return "index";
+	}
+	
+	@RequestMapping("/create_order")
+	@ResponseBody
+	public String CreateOrder(@RequestBody Map<String, Object>data) throws RazorpayException
+	{
+//		System.out.print(data);
+//		System.out.println("Order Function called");
+		int amount =Integer.parseInt(data.get("amount").toString());
+		
+		RazorpayClient client = new RazorpayClient("rzp_test_bgKFJsFoxd29mf","yuvOXfrjYFyaJ8q8lsPdGsiu");
+		//code taken from razorpay site to create order
+		JSONObject orderRequest = new JSONObject();
+		orderRequest.put("amount",amount*100); //amount taken is in paise
+		orderRequest.put("currency","INR");
+		orderRequest.put("receipt", "receipt#1");
+
+//		creating new order
+		Order order=client.orders.create(orderRequest);
+		System.out.println(order);
+		
+		
+		
+		
+		return order.toString();
+	}
+}
---
+++ src/main/resources/static/JS/myjs.js
@@ -0,0 +1,84 @@
+const paymentStart = () => {
+    console.log("payment started");
+    var amount = $("#paymentfield").val();
+    console.log(amount);
+    if (amount == "" || amount == null || amount < 1) {
+       
+	        Swal.fire({
+	  icon: "error",
+	  title: "Oops...",
+	  text: "Amount must be Greater than 1",
+	  
+	});
+        return;
+    }
+
+    //we will use ajax to send request to server
+    $.ajax(
+        {
+            url: '/create_order',
+            data: JSON.stringify({ amount: amount, info: "Order_requested" }),
+            contentType: 'application/json',
+            type: 'POST',
+            dataType: 'json',
+            success: function (response) {
+                if (response.status == "created") {
+                    //open payment form
+                    let options = {
+                        key: 'rzp_test_bgKFJsFoxd29mf',
+                        amount: response.amount,
+                        currency: 'INR',
+                        name: 'Payment Gateway App Donation',
+                        description: 'Donation',
+                        image: 'ghjbkj',
+                        order_id:response.id,
+                        "handler":function(response)
+                        {
+                            console.log(response.razorpay_payment_id)
+                            console.log(response.razorpay_order_id)
+                            console.log(response.razorpay_signature)
+                            console.log('payment successfull')
+                            Swal.fire({
+                                position: "top",
+                                icon: "success",
+                                title: "Payment Successful",
+                                showConfirmButton: false,
+                                timer: 1500
+                              });
+                        },
+                        prefill:{
+                            name:"",
+                            email:"",
+                            contact:"",
+                        },
+                        theme:{
+                            color:"black"
+                        },
+
+                    };
+                    let rzp=new Razorpay(options);
+                    rzp.on('payment.failed', function (response){
+                        console.log(response.error.code);
+                        console.log(response.error.description);
+                        console.log(response.error.source);
+                        console.log(response.error.step);
+                        console.log(response.error.reason);
+                        console.log(response.error.metadata.order_id);
+                        console.log(response.error.metadata.payment_id);
+                       Swal.fire({
+								  icon: "error",
+								  title: "OOPS...",
+								  text: "Payment failed",
+							  
+								});
+                });
+                    rzp.open();
+                }
+                console.log(response)
+            },
+            error: function (error) {
+                alert("something went wrong")
+            }
+        }
+    )
+}
